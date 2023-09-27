3b9591f6fbaf6766d48cb3dfe9e09428e2cfca8c
kurtymd <kurtimdmitruk@gmail.com>
2023-09-27 20:11:57 +0200

    L5

--- Memorize/MemorizeApp.swift
+++ Memorize/MemorizeApp.swift
@@ -2,18 +2,7 @@
 //  MemorizeApp.swift
 //  Memorize
 //
-//  Created by Bohdan on 03.09.2023.
+//  Created by Bohdan on 27.09.2023.
 //
 
-import SwiftUI
-
-@main
-struct MemorizeApp: App {
-    @StateObject var game = EmojiMemoryGame()
-    
-    var body: some Scene {
-        WindowGroup {
-            EmojiMemoryGameView(viewModel: game)
-        }
-    }
-}
+import Foundation
